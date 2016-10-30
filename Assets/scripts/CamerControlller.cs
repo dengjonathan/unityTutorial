8d2d209f9cd26e8e3e4d1d3db1173432f049e44c
jonathan.deng <dengjonathan@gmail.com>
2016-10-30 00:16:37 -0400

    completed Unity tutorial

---
+++ Assets/scripts/CamerControlller.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamerControlller : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	// late update guaranteed to run at end of frame so camera is not updated until player is done updating
+	void LateUpdate() {
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Text countText;
+	public Text winText;
+	private int count;
+	private Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText();
+		winText.text = "";
+	}
+	void Update() {} //applied before any frame is rendered
+
+
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce(movement * speed);
+	} // applied before any physics update
+
+	void OnTriggerEnter(Collider other) {
+		print(other.tag);
+		if (other.gameObject.CompareTag("Pickup")) {
+			other.gameObject.SetActive(false);
+			count += 1;
+			SetCountText();
+			if (count >= 7) {
+				winText.text = "You won!!!";
+			}
+		}
+	}
+
+	void SetCountText() {
+		countText.text = "Count:" + count.ToString();
+	}
+}
